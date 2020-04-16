0ac22d4b23a4ef6403f8ffe435dde6ce53ef6773
Patrick Siekmeier <siekmeier.p@gmail.com>
2020-04-16 15:03:15 +0200

    added some basic content

--- basics/arrays.py
+++ basics/arrays.py
@@ -1,5 +1,23 @@
-mylist = [1,2,3]
-anotherList = ['stringsadg',1,2,3,23.2,True,'asdf',mylist]
+# mylist = [1,2,3]
+# anotherList = ['stringsadg','1','2','3','23.2','True','asdf',"Abc",'mylist']
+# print(anotherList)
+#
+# anotherList.pop()
+#
+# print(anotherList[::-1])
+# anotherList.reverse()
+# print(anotherList)
+#
+# anotherList.sort()
+# print(anotherList)
 
-print(len(anotherList[0]))
-print(mylist[-2])
+mylist = [1,2,['x','y','z']]
+letters = mylist[2]
+y = mylist[2][1]
+
+print(letters)
+print(y)
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+first_col = [row[0] for row in matrix]
+print(first_col)
