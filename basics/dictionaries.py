d6a1d020528b0d83670aa11b0f91d3df060880cd
Patrick Siekmeier <siekmeier.p@gmail.com>
2020-04-16 15:30:18 +0200

    first assignment finished

---
+++ basics/dictionaries.py
@@ -0,0 +1,7 @@
+# my_stuff = {"key1":"value1","key2":"value2"}
+#
+# print(my_stuff["key1"])
+
+my_stuff = {"key1":123,"key2":"value2","key3":{"123":[1,2,'grabMe']}}
+
+print(my_stuff["key3"]["123"][2])
---
+++ basics/tupleSetBoolean.py
@@ -0,0 +1,29 @@
+## Booleans
+True
+False
+
+## Tuples
+t = (1,2,3)
+print(t[0])
+
+# t[1] = "Two"
+# print(t)
+
+my_list = [1,2,3]
+my_list[1] = "two"
+print(my_list)
+
+## Set
+
+x = set()
+x.add(1)
+x.add(2)
+print(x)
+x.add(3)
+x.add(2)
+print(x)
+
+another_list = [1,2,2,3,4,4,4,5]
+print(another_list)
+my_set = set(another_list)
+print(my_set)
