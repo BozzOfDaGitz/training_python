62973166ae4b05c42251b5daa34e70be76a7711f
Patrick Siekmeier <siekmeier.p@gmail.com>
2020-04-21 14:25:51 +0200

    use templates in ProTwo

--- django/ProTwo/AppTwo/views.py
+++ django/ProTwo/AppTwo/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<em>My Second App</em>')
+    return HttpResponse('Welcome to the index page!')
+
+def help(request):
+    context_dict = {'help':'Help Page'}
+    return render(request,'AppTwo/help.html',context=context_dict)
