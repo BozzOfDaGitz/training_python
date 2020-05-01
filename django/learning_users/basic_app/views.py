718b3356fc9b1ce04897d4b3e7f85b36f4676b2c
Patrick Siekmeier <siekmeier.p@gmail.com>
2020-05-01 17:33:51 +0200

    add user registration forms and html

--- django/learning_users/basic_app/views.py
+++ django/learning_users/basic_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from basic_app import forms
 
 # Create your views here.
+def index(request):
+    return render(request,'basic_app/index.html')
+
+def register(request):
+    user_form = forms.UserForm()
+    profile_form = forms.UserProfileInfoForm()
+    context_dict = {'user_form':user_form,'profile_form':profile_form,'registered':False}
+    return render(request,'basic_app/register.html',context=context_dict)
