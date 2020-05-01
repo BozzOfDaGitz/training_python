81d9de1e90b175e5504e49fd03779b46776324a4
Patrick Siekmeier <siekmeier.p@gmail.com>
2020-05-01 18:35:41 +0200

    try to make saving of new user work

--- django/learning_users/basic_app/views.py
+++ django/learning_users/basic_app/views.py
@@ -8,5 +8,12 @@ def index(request):
 def register(request):
     user_form = forms.UserForm()
     profile_form = forms.UserProfileInfoForm()
+
+    if request.method == 'POST':
+        user_form = forms.UserForm(request.POST)
+        profile_form = forms.UserProfileInfoForm(request.POST)
+        if user_form.is_valid() & profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
     context_dict = {'user_form':user_form,'profile_form':profile_form,'registered':False}
     return render(request,'basic_app/register.html',context=context_dict)
