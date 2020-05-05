2382876f3e2c93c2a6c70d464d0c4ed0f8d0c846
Patrick Siekmeier <siekmeier.p@gmail.com>
2020-05-05 15:44:47 +0200

    add create, update and delete views

--- django/CBVs_project/basic_app/views.py
+++ django/CBVs_project/basic_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView,ListView,DetailView
+from django.views.generic import (View,TemplateView,ListView,DetailView,
+                                    CreateView,UpdateView,DeleteView)
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from basic_app import models
 
 # Create your views here.
@@ -21,3 +23,15 @@ class SchoolListView(ListView):
 class SchoolDetailView(DetailView):
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ['name','principal','location']
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = ['name','principal']
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('basic_app:list')
